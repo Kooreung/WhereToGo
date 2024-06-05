72a5516ebe83fd6cedc690f657eeb4ad2449da34
kooreung <crisishyun@gmail.com>
2024-06-05 21:22:28 +0900

    feat : React NavBar 초안 생성
    NavBar 기본 설정 빈 파일 생성

---
+++ frontend/src/component/Navbar.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { Box, Center, Flex } from "@chakra-ui/react";
+
+function Navbar() {
+  return (
+    <Flex
+      align={"center"}
+      justify={"space-between"}
+      px={6}
+      mb={6}
+      border={"red dotted 1px"}
+      height={20}
+    >
+      <Flex gap={3}>
+        <Box>로고</Box>
+        <Box>홈페이지 제목</Box>
+      </Flex>
+      <Center gap={12}>
+        <Box>MD Pick</Box>
+        <Box>회원 게시판</Box>
+      </Center>
+      <Flex gap={3}>
+        <Box>회원가입/목록</Box>
+        <Box>로그인/아웃</Box>
+        <Box>프로필</Box>
+      </Flex>
+    </Flex>
+  );
+}
+
+export default Navbar;
