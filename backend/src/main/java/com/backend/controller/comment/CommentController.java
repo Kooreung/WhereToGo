b7e69f7a80be70b5c78b9caeda74e0142ceea21c
sunghwan <simcom0820@naver.com>
2024-06-05 15:40:14 +0900

    feat:Comment 생성
    기본 Controller, Mapper, Service , Entity 추가

--- backend/src/main/java/com/backend/controller/comment/CommentController.java
+++ backend/src/main/java/com/backend/controller/comment/CommentController.java
@@ -1,4 +1,32 @@
 package com.backend.controller.comment;
 
+import com.backend.service.comment.CommentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/comment")
 public class CommentController {
+    final CommentService service;
+
+    @PostMapping("add")
+    public void getComment() {
+        service.add();
+    }
+
+    @PostMapping("list/{postId}")
+    public void getCommentList() {
+        service.list();
+    }
+
+    @PutMapping("edit")
+    public void getCommentEdit() {
+        service.edit();
+    }
+
+    @DeleteMapping("delete")
+    public void deleteComment() {
+        service.delete();
+    }
 }
--- backend/src/main/java/com/backend/mapper/comment/CommentMapper.java
+++ backend/src/main/java/com/backend/mapper/comment/CommentMapper.java
@@ -1,4 +1,15 @@
 package com.backend.mapper.comment;
 
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
 public interface CommentMapper {
+    void insert();
+
+    void selectByPostId();
+
+    void update();
+
+    void delete();
+
 }
