fffa2db0d794b17ec7531fe158a26e4b0081e4f7
kooreung <crisishyun@gmail.com>
2024-06-07 22:52:55 +0800

    feat : Custom Style 생성
    작업 시 활용 할 가이드라인 생성

--- frontend/src/page/Lobby.jsx
+++ frontend/src/page/Lobby.jsx
@@ -1,16 +1,29 @@
 import React from "react";
 import { Box, Center, Flex } from "@chakra-ui/react";
-import { GuideLine } from "../App.jsx";
+import {
+  GuideLineLargeBanner,
+  GuideLineMediumBanner,
+} from "../CustomStyles.jsx";
 
 function Lobby() {
   return (
     <Box>
-      <Center>
-        <Flex {...GuideLine}>원형 메뉴</Flex>
+      <Center my={6}>
+        <Flex {...GuideLineMediumBanner}>원형 메뉴</Flex>
       </Center>
-      <Flex>
-        <Box fontSize="3xl">MD 추천 Pick</Box>
-        <Flex {...GuideLine}>MD 메뉴</Flex>
+      <Box fontSize="3xl" pl={40}>
+        MD 추천 Pick
+      </Box>
+      <Flex direction={"column"} align={"center"} my={6}>
+        <Flex {...GuideLineLargeBanner}>MD 메뉴</Flex>
+      </Flex>
+      <Box fontSize="3xl" pl={80}>
+        회원 인기글
+      </Box>
+      <Flex direction={"column"} align={"center"} my={6}>
+        <Box {...GuideLineMediumBanner}></Box>
+        <Box {...GuideLineMediumBanner}></Box>
+        <Box {...GuideLineMediumBanner}></Box>
       </Flex>
     </Box>
   );
