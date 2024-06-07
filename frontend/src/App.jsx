b83596b5a60c8a8e8bf2f0d0c084d98c08e3d063
kooreung <crisishyun@gmail.com>
2024-06-07 22:45:16 +0800

    feat : Custom Style 생성
    작업 시 활용 할 가이드라인 생성

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,6 +14,18 @@ const router = createBrowserRouter([
   },
 ]);
 
+// 작업 시 사용 할 가이드라인용 스타일 값
+export const GuideLine = {
+  align: "center",
+  justify: "center",
+  px: 6,
+  mb: 6,
+  border: "red dotted 1px",
+  bg: "lightgray",
+  w: "75%",
+  h: 200,
+};
+
 function App() {
   return (
     // <LoginProvider>
--- frontend/src/page/Lobby.jsx
+++ frontend/src/page/Lobby.jsx
@@ -1,8 +1,19 @@
 import React from "react";
-import { Center } from "@chakra-ui/react";
+import { Box, Center, Flex } from "@chakra-ui/react";
+import { GuideLine } from "../App.jsx";
 
 function Lobby() {
-  return <Center>메인 로비</Center>;
+  return (
+    <Box>
+      <Center>
+        <Flex {...GuideLine}>원형 메뉴</Flex>
+      </Center>
+      <Flex>
+        <Box fontSize="3xl">MD 추천 Pick</Box>
+        <Flex {...GuideLine}>MD 메뉴</Flex>
+      </Flex>
+    </Box>
+  );
 }
 
 export default Lobby;
