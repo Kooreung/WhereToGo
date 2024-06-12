932cce24d70acb51287dd0999a3f3e70dddc8820
kooreung <crisishyun@gmail.com>
2024-06-12 20:44:17 +0900

    feat : Post Write 게시글 작성 부수 기능
    주석 처리
    저장 버튼 클릭 시 Modal
    취소 버튼 클릭 시 Modal

--- frontend/src/page/post/PostWrite.jsx
+++ frontend/src/page/post/PostWrite.jsx
@@ -6,7 +6,15 @@ import {
   FormControl,
   FormLabel,
   Input,
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
   Textarea,
+  useDisclosure,
+  useToast,
 } from "@chakra-ui/react";
 import { GuideLineMediumBanner } from "../../CustomStyles.jsx";
 import axios from "axios";
@@ -17,24 +25,52 @@ function PostWrite() {
   const [content, setContent] = useState("");
   const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
+  const toast = useToast();
+  const {
+    isOpen: isModalOpenOfSave,
+    onOpen: onModalOpenOfSave,
+    onClose: onModalCloseOfSave,
+  } = useDisclosure();
+  const {
+    isOpen: isModalOpenOfCancel,
+    onOpen: onModalOpenOfCancel,
+    onClose: onModalCloseOfCancel,
+  } = useDisclosure();
 
-  function handleClickSave() {
-    setLoading(true);
-    axios
-      .postForm("/api/post/add", { title, content })
-      .then(() => {
-        navigate("/");
-      })
-      .catch()
-      .finally(() => setLoading(false));
-  }
-
+  // 저장 버튼 비활성화 조건
   let disableSaveButton = false;
   if (title.trim().length === 0) {
     disableSaveButton = true;
   }
   if (content.trim().length === 0) {
     disableSaveButton = true;
+  }
+
+  // 저장 버튼 클릭 시
+  function handleClickSave() {
+    setLoading(true);
+    axios
+      .postForm("/api/post/add", { title, content })
+      .then(() => {
+        navigate(`/post/list`);
+        toast({
+          status: "success",
+          position: "bottom",
+          description: "게시글이 등록되었습니다.",
+        });
+      })
+      .catch()
+      .finally(() => setLoading(false));
+  }
+
+  // 취소 버튼 클릭 시
+  function handleClickCancel() {
+    navigate("/post/list");
+    toast({
+      status: "info",
+      position: "bottom",
+      description: "게시글 작성이 취소되었습니다.",
+    });
   }
 
   return (
@@ -79,19 +115,48 @@ function PostWrite() {
           <Box>
             <Box align={"left"} my={10}>
               <Button
-                onClick={handleClickSave}
+                onClick={onModalOpenOfSave}
                 isLoading={loading}
                 isDisabled={disableSaveButton}
               >
                 등록
               </Button>
-              <Button>취소</Button>
-              {/* Todo 게시글 작성 중 취소 필요 */}
+              <Button onClick={onModalOpenOfCancel}>취소</Button>
               {/* Todo 게시글 작성 중 임시저장 필요 */}
             </Box>
           </Box>
         </Box>
       </Flex>
+
+      {/* 게시글 작성 저장 Modal */}
+      <Modal isOpen={isModalOpenOfSave} onClose={onModalCloseOfSave}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>게시글 저장</ModalHeader>
+          <ModalBody>게시글을 등록하시겠습니까?</ModalBody>
+          <ModalFooter>
+            <Flex>
+              <Button onClick={handleClickSave}>확인</Button>
+              <Button onClick={onModalCloseOfSave}>취소</Button>
+            </Flex>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+
+      {/* 게시글 작성 취소 Modal */}
+      <Modal isOpen={isModalOpenOfCancel} onClose={onModalCloseOfCancel}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>게시글 작성 취소</ModalHeader>
+          <ModalBody>작성을 취소하시겠습니까?</ModalBody>
+          <ModalFooter>
+            <Flex>
+              <Button onClick={handleClickCancel}>확인</Button>
+              <Button onClick={onModalCloseOfCancel}>취소</Button>
+            </Flex>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </Box>
   );
 }
