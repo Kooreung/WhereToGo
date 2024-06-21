2e52ed0d0e9dfce1d7965f3adfded81c841b5548
sunghwan <simcom0820@naver.com>
2024-06-21 09:17:24 +0900

    feat: Lobby 지역 리스트
    Lobby PlaceList 레이아웃 정리, 애니메이션 추가

--- frontend/src/component/Lobby/LobbyPlaceList.jsx
+++ frontend/src/component/Lobby/LobbyPlaceList.jsx
@@ -1,11 +1,11 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import {
   Avatar,
   Box,
   Button,
   Center,
+  Flex,
   Stack,
-  Wrap,
   WrapItem,
 } from "@chakra-ui/react";
 import { useNavigate } from "react-router-dom";
@@ -61,43 +61,60 @@ const places = [
 ];
 
 export function LobbyPlaceList() {
+  const dataRef = useRef(null);
+  const [positionX, setPositionX] = useState(0);
   const navigate = useNavigate();
 
-  function handleMoveRight() {}
+  function handleMoveRight() {
+    const flexWidth = dataRef.current.scrollWidth;
+    const containerWidth = dataRef.current.parentElement.offsetWidth;
+    setPositionX((prev) => Math.max(prev - 125, containerWidth - flexWidth));
+  }
 
-  function handleMoveLeft() {}
+  function handleMoveLeft() {
+    setPositionX((prev) => Math.min(prev + 125, 0));
+  }
 
   return (
-    <Box border={"5px solid green"}>
-      <Wrap spacing="20px" border={"5px solid red"}>
+    <Box border={"1px solid green"}>
+      <Flex>
         <Center>
-          <Button border={"1px solid black"} onClick={handleMoveLeft}>
-            옆
-          </Button>
+          <Button onClick={handleMoveLeft}>옆</Button>
         </Center>
-        {places.slice(0, 5).map((place, index) => (
-          <WrapItem key={index} border={"5px solid blue"}>
-            <Box width="100px" border={"5px solid black"}>
-              {/* 조정 가능한 너비 */}
-              <Stack
-                spacing={2}
-                align="center"
-                onClick={() =>
-                  navigate(`/post/list?type=all&keyword=${place.keyword}`)
-                }
-              >
-                <Avatar size="xl" name="Dan Abrahmov" src={place.src} />
-                <Box textAlign="center">{place.name}</Box>
-              </Stack>
-            </Box>
-          </WrapItem>
-        ))}
+        <Box w={"554px"} overflow={"hidden"} border={"1px solid black"}>
+          <Flex
+            spacing="20px"
+            border={"5px solid red"}
+            ref={dataRef}
+            sx={{
+              transform: `translateX(${positionX}px)`,
+              transition: "transform 0.5s ease",
+            }}
+          >
+            {places.map((place, index) => (
+              <WrapItem key={index} border={"5px solid blue"}>
+                <Box width="100px" border={"5px solid black"}>
+                  {/* 조정 가능한 너비 */}
+                  <Stack
+                    spacing={2}
+                    align="center"
+                    onClick={() =>
+                      navigate(`/post/list?type=all&keyword=${place.keyword}`)
+                    }
+                  >
+                    <Avatar size="xl" name="Dan Abrahmov" src={place.src} />
+                    <Box textAlign="center">{place.name}</Box>
+                  </Stack>
+                </Box>
+              </WrapItem>
+            ))}
+          </Flex>
+        </Box>
+
         <Center>
-          <Button border={"1px solid black"} onClick={handleMoveRight}>
-            옆
-          </Button>
+          <Button onClick={handleMoveRight}>옆</Button>
         </Center>
-      </Wrap>
+      </Flex>
     </Box>
   );
 }
