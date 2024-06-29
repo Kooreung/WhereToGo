57d951959acc5bc3685d639313c341e55f66a94e
kooreung <crisishyun@gmail.com>
2024-06-29 11:28:35 +0800

    feat : Lobby CSS
    Footer 위치

--- frontend/src/page/Home.jsx
+++ frontend/src/page/Home.jsx
@@ -1,14 +1,21 @@
 import React from "react";
-import { Flex } from "@chakra-ui/react";
+import { Flex, Spacer } from "@chakra-ui/react";
 import { Outlet } from "react-router-dom";
 import Navbar from "../component/Navbar.jsx";
 import Footer from "../component/Footer.jsx";
 
 function Home() {
   return (
-    <Flex direction="column" justify="center" alignItems="center">
+    <Flex
+      direction="column"
+      justify="start"
+      alignItems="center"
+      minH={"100vh"}
+      border={"1px dotted red"}
+    >
       <Navbar />
       <Outlet />
+      <Spacer />
       <Footer />
     </Flex>
   );
