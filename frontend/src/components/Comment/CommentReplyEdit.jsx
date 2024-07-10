1abf181dd3adc2ee26402484164c5497db072ffb
sunghwan <simcom0820@naver.com>
2024-07-10 12:01:48 +0900

    fix:replycomment 대댓글 수정 ,삭제 모달 추가

--- frontend/src/components/Comment/CommentReplyEdit.jsx
+++ frontend/src/components/Comment/CommentReplyEdit.jsx
@@ -56,6 +56,30 @@ export function CommentReplyEdit({
       });
   }
 
+  function handleEditReplyKeyDown(e) {
+    if (e.key === "Enter" && !e.shiftKey) {
+      const trimmedReplyComment = reWriteComment.trim();
+      const trimmedOriginalComment = replyComment.replyComment.trim();
+
+      if (
+        trimmedReplyComment !== trimmedOriginalComment && // 제거된 reWriteComment가 원래 코멘트와 다르고
+        trimmedReplyComment.length > 0 // 제거된 reWriteComment의 길이가 0보다 크면
+      ) {
+        onOpen();
+      } else {
+        toast({
+          status: "error",
+          isClosable: true,
+          description: "이전 댓글과 동일합니다",
+          position: "bottom",
+        });
+      }
+
+      e.preventDefault();
+      e.stopPropagation();
+    }
+  }
+
   return (
     <Box>
       <Box>
@@ -65,6 +89,7 @@ export function CommentReplyEdit({
             mb={4}
             value={reWriteComment}
             onChange={(e) => setReWriteComment(e.target.value)}
+            onKeyDown={handleEditReplyKeyDown}
           />
         </Flex>
         <Box>
@@ -77,8 +102,8 @@ export function CommentReplyEdit({
       <Modal isOpen={isOpen} onClose={onClose}>
         <ModalOverlay />
         <ModalContent>
-          <ModalHeader>작성 확인</ModalHeader>
-          <ModalBody>작성하시겠습니까?</ModalBody>
+          <ModalHeader>수정 확인</ModalHeader>
+          <ModalBody>수정하시겠습니까?</ModalBody>
           <ModalFooter>
             <Button onClick={handleSubmitEditReply}>확인</Button>
             <Button onClick={onClose}>취소</Button>
