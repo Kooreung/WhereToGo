14cf6400ce285318c9ec90b2048d15501cf092ea
mj <wmj0924@gmail.com>
2024-07-12 10:10:23 +0900

    fix : 채팅 완(css 빼고 ㅠ)

--- frontend/src/component/Chat/ChatWebSocket.jsx
+++ frontend/src/component/Chat/ChatWebSocket.jsx
@@ -10,7 +10,7 @@ import {
   useColorModeValue,
   VStack,
 } from "@chakra-ui/react";
-import {useNotifications} from "./NotificationProvider.jsx";
+import { useNotifications } from "./NotificationProvider.jsx";
 
 export function ChatWebSocket({ roomInfo }) {
   //웹소켓 연결 객체
@@ -25,9 +25,10 @@ export function ChatWebSocket({ roomInfo }) {
   // 새 메시지 도착 여부를 위한 상태 변수
   const [isNewMessage, setIsNewMessage] = useState(false);
   const [newMessage, setNewMessage] = useState("");
+  const [myMessage, setMyMessage] = useState("");
 
   const { notifications, addNotification, removeNotification } =
-      useNotifications();
+    useNotifications();
 
   const connect = (roomInfo) => {
     //웹소켓 연결
@@ -41,11 +42,11 @@ export function ChatWebSocket({ roomInfo }) {
           //누군가 발송했던 메시지를 리스트에 추가
           const nowMessage = JSON.parse(message.body);
           setNewMessage(message.body);
-          console.log(nowMessage.state);
+          setMyMessage(message.body);
+          console.log("dsd" + nowMessage.memberId);
           setMessages((prevMessages) => [...prevMessages, nowMessage]);
           if (nickName === nowMessage.name) {
             setIsNewMessage(false);
-            scrollToBottom();
           } else {
             setIsNewMessage(true);
           }
@@ -90,7 +91,6 @@ export function ChatWebSocket({ roomInfo }) {
 
   //메세지 전송
   const sendMessage = () => {
-    scrollToBottom();
     if (stompClient.current && inputValue) {
       //현재로서는 임의의 테스트 값을 삽입
       const body = {
@@ -102,6 +102,8 @@ export function ChatWebSocket({ roomInfo }) {
       stompClient.current.send(`/pub/message`, {}, JSON.stringify(body));
       setInputValue("");
     }
+    // 사용자가 메시지를 보낸 경우, 새 메시지 버튼을 표시하지 않음
+    setShowScrollButton(false);
   };
 
   useEffect(() => {
@@ -115,6 +117,13 @@ export function ChatWebSocket({ roomInfo }) {
 
     return () => disconnect();
   }, [roomInfo]); // roomInfo를 의존성 배열에 추가합니다.
+
+  useEffect(() => {
+    // 메시지 목록을 가져온 후 스크롤을 맨 아래로 이동
+    fetchMessages(roomInfo.chatRoomId).then(() => {
+      scrollToBottom("auto");
+    });
+  }, [roomInfo]);
 
   const Message = ({ userRead, message, isOwnMessage }) => {
     const align = isOwnMessage ? "flex-end" : "flex-start";
@@ -138,8 +147,12 @@ export function ChatWebSocket({ roomInfo }) {
   const [showScrollButton, setShowScrollButton] = useState(false);
 
   // 스크롤을 아래로 이동시키는 함수
-  const scrollToBottom = () => {
-    messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
+  const scrollToBottom = (type) => {
+    if (type == "s") {
+      messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
+    } else {
+      messagesEndRef.current?.scrollIntoView({ behavior: "auto" });
+    }
     setShowScrollButton(false);
   };
 
@@ -161,16 +174,19 @@ export function ChatWebSocket({ roomInfo }) {
     fetchMessages(roomInfo.chatRoomId);
     if (isNewMessage) {
       //스크롤을 제일 밑으로 내린 길이의 - 800인 위치에서 메세지를 보고있을때 메세지가 새로오면 새로운 메세지 버튼 띄우기
-
       if (scrollPosition >= scrollHeight) {
-        scrollToBottom();
+        scrollToBottom("s");
       } else {
         // 그렇지 않으면 새 메시지 알림 버튼 표시
         setShowScrollButton(true);
       }
       setIsNewMessage(false);
     }
-  }, [newMessage,notifications]);
+  }, [notifications]);
+
+  useEffect(() => {
+    scrollToBottom("s");
+  }, [myMessage]);
 
   //----------------------------------------
 
@@ -205,7 +221,7 @@ export function ChatWebSocket({ roomInfo }) {
           position="absolute"
           bottom="80px"
           right="95px"
-          onClick={scrollToBottom}
+          onClick={scrollToBottom("s")}
         >
           새 메시지 보기
         </Button>
